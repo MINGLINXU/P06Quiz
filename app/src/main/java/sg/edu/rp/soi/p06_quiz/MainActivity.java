51340453673aea613cd8ba90e8a65741666c2b03
XUMINGLIN <18046506@myrp.edu.sg>
2019-05-24 15:59:36 +0800

    L06QUIZ_XUMINGLIN

--- app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
+++ app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
@@ -2,16 +2,71 @@ package sg.edu.rp.soi.p06_quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
+    TextView tvOperation;
+    TextView tvResult;
+    EditText num1;
+    EditText num2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvOperation = findViewById(R.id.tvOperation);
+        tvResult = findViewById(R.id.result);
+        num1 = findViewById(R.id.etNum1);
+        num2 = findViewById(R.id.etNum2);
+
+
+        registerForContextMenu(tvOperation);
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.add(0,0,0,"+");
+        menu.add(0,1,1,"-");
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        String value1 = num1.getText().toString();
+        int number1 = Integer.parseInt(value1);
+        String value2 = num2.getText().toString();
+        int number2 = Integer.parseInt(value2);
+        if(item.getItemId()==0) { //check whether the selected menu item ID is 0
+            //code for action
+            int total = number1+ number2;
+            tvResult.setText(String.valueOf(total));
+
+
+            return true; //menu item successfully handled
+        }
+        else if(item.getItemId()==1) { //check if the selected menu item ID is 1
+            //code for action
+            int total = number1- number2;
+            tvResult.setText(String.valueOf(total));
+            return true;  //menu item successfully handled
+        }
+
+        return super.onContextItemSelected(item); //pass menu item to the superclass implementation
+    }
 
 }
